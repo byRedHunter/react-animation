f3cbde8208bfb0e5d60d0868d6dcbf2b71759040
Jhonny Quispe <jhonny.quispejl@gmail.com>
2020-08-15 16:23:36 -0500

    Updates

---
+++ precache-manifest.5882239b3bf7a0de6033a488c829d65c.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0e89a10f57ae1eb2e20013b8784c945",
+    "url": "/react-animation/index.html"
+  },
+  {
+    "revision": "3c3dca2da0d69fa9b08e",
+    "url": "/react-animation/static/css/main.bbafaa86.chunk.css"
+  },
+  {
+    "revision": "06cf1cfdc0f9fa8e5388",
+    "url": "/react-animation/static/js/2.cb0362d4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-animation/static/js/2.cb0362d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c3dca2da0d69fa9b08e",
+    "url": "/react-animation/static/js/main.3d7f7ab1.chunk.js"
+  },
+  {
+    "revision": "3d56f43eb8a2fcb621d1",
+    "url": "/react-animation/static/js/runtime-main.1bb63bb4.js"
+  },
+  {
+    "revision": "e9b52209dbaac935c802f5ed9b4102ef",
+    "url": "/react-animation/static/media/background.e9b52209.png"
+  },
+  {
+    "revision": "6d0f38aaa66022eef8477e62b86c8283",
+    "url": "/react-animation/static/media/background2x.6d0f38aa.png"
+  },
+  {
+    "revision": "45598562077bdc84281b5330ff4d283e",
+    "url": "/react-animation/static/media/corazon.45598562.png"
+  },
+  {
+    "revision": "ba2e415e817351fb402d07afd2be5ceb",
+    "url": "/react-animation/static/media/easter-a.ba2e415e.png"
+  },
+  {
+    "revision": "7d7a81dcaa54ebefe86d47f90c6bb589",
+    "url": "/react-animation/static/media/easter-b.7d7a81dc.png"
+  },
+  {
+    "revision": "0db2afe8201f90349fca3d76da2019ef",
+    "url": "/react-animation/static/media/icomoon.0db2afe8.ttf"
+  },
+  {
+    "revision": "85c2eed24cd21f70d9880d00da02f6bd",
+    "url": "/react-animation/static/media/icomoon.85c2eed2.eot"
+  },
+  {
+    "revision": "9f215c1e5e1c89f82c1aff021398d605",
+    "url": "/react-animation/static/media/icomoon.9f215c1e.svg"
+  },
+  {
+    "revision": "e197b52e37ffa7f2e2e6e3d4562c9da4",
+    "url": "/react-animation/static/media/icomoon.e197b52e.woff"
+  },
+  {
+    "revision": "25c6f861860963cb105716cc999b5174",
+    "url": "/react-animation/static/media/invie-acustica.25c6f861.png"
+  },
+  {
+    "revision": "e039fff97126b1b18d68f4be45bf6d25",
+    "url": "/react-animation/static/media/invie-classic.e039fff9.png"
+  },
+  {
+    "revision": "70297162b2ac4eb6ac54a0ddd5f85bfe",
+    "url": "/react-animation/static/media/team-platzi-azul.70297162.jpg"
+  }
+]);
